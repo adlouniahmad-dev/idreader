0af5c772bae9740d323373f7965b84ef3199896b
Ahmad Adlouni <adlouniahmad@outlook.com>
2018-04-26 16:36:29 +0300

    DONE

---
+++ src/Entity/Appointment.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AppointmentRepository")
+ */
+class Appointment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $applicantName;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $applicantSsn;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $premiseOwner;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $time;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApplicantName()
+    {
+        return $this->applicantName;
+    }
+
+    /**
+     * @param mixed $applicantName
+     */
+    public function setApplicantName($applicantName): void
+    {
+        $this->applicantName = $applicantName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApplicantSsn()
+    {
+        return $this->applicantSsn;
+    }
+
+    /**
+     * @param mixed $applicantSsn
+     */
+    public function setApplicantSsn($applicantSsn): void
+    {
+        $this->applicantSsn = $applicantSsn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPremiseOwner()
+    {
+        return $this->premiseOwner;
+    }
+
+    /**
+     * @param mixed $premiseOwner
+     */
+    public function setPremiseOwner($premiseOwner): void
+    {
+        $this->premiseOwner = $premiseOwner;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param mixed $time
+     */
+    public function setTime($time): void
+    {
+        $this->time = $time;
+    }
+
+}
---
+++ src/Entity/Token.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TokenRepository")
+ */
+class Token
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $token;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param mixed $token
+     */
+    public function setToken($token): void
+    {
+        $this->token = $token;
+    }
+}
